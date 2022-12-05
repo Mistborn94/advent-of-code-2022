a3cf25a5172981650f0d91b0c5779bff1d76c4b5
Renette Ros <supergirl0894+git@gmail.com>
2022-12-05 07:40:53 +0200

    Solve day 5
    Signed-off-by: Renette Ros <supergirl0894+git@gmail.com>

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -1,10 +1,44 @@
 package day5
 
-fun solveA(text: String): Int {
-    return 0
+import java.util.*
+
+val pattern = "move (\\d+) from (\\d+) to (\\d+)".toRegex()
+
+fun solveA(text: String): String = solve(text) { stacks, count, from, to ->
+    repeat(count) {
+        val pop = stacks[from - 1].pop()
+        stacks[to - 1].push(pop)
+    }
+}
+
+fun solveB(text: String): String = solve(text) { stacks, count, from, to ->
+    val pop = (0 until count).map { stacks[from - 1].pop() }
+    stacks[to - 1].addAll(pop.reversed())
+}
+
+private fun solve(text: String, handler: (List<Stack<Char>>, Int, Int, Int) -> Unit): String {
+    val (start, instr) = text.split("\n\n")
+
+    val stacks = start.lines().last().chunked(4).map<String, Stack<Char>> { Stack() }
+
+    start.lines()
+        .dropLast(1)
+        .reversed()
+        .forEach { line ->
+            line.chunked(4).forEachIndexed { index, crate ->
+                if (crate[1] != ' ') {
+                    stacks[index].push(crate[1])
+                }
+            }
+        }
+
+    instr.lines().forEach {
+        val matchResult = pattern.matchEntire(it)
+        val (count, from, to) = matchResult!!.groupValues.drop(1).map(String::toInt)
+
+        handler(stacks, count, from, to)
+    }
+    return stacks.map { it.peek() }.joinToString(separator = "")
 }
 
 
-fun solveB(text: String): Int {
-    return 0
-}
