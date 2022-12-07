e3ccbedd3ac07ac64b65100b1515fce47b63448f
Renette Ros <supergirl0894+git@gmail.com>
2022-12-07 07:29:05 +0200

    Clean up duplication in day 7
    Signed-off-by: Renette Ros <supergirl0894+git@gmail.com>

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -2,36 +2,10 @@ package day7
 
 import java.util.*
 
-//Find all of the directories with a total size of at most 100000.
+//Find all the directories with a total size of at most 100000.
 // What is the sum of the total sizes of those directories?
 fun solveA(text: String): Int {
-    val dirStack = Stack<String>()
-    var dirPath = ""
-    val directories = mutableMapOf<String, Int>()
-    val subDirectories = mutableMapOf<String, MutableList<String>>()
-
-    text.lines().forEach { line ->
-        if (line.startsWith("\$ cd")) {
-            if (line.endsWith("/")) {
-                dirStack.clear()
-                dirStack.push("/")
-            } else if (line.endsWith("..")) {
-                dirStack.pop()
-            } else {
-                dirStack.push(line.substring(5))
-            }
-            dirPath = dirStack.joinToString(separator = "/")
-            directories.putIfAbsent(dirPath, 0)
-            subDirectories.putIfAbsent(dirPath, mutableListOf())
-        } else if (line.startsWith("\$ ls")) {
-            //do nothing
-        } else if (line.startsWith("dir")) {
-            subDirectories[dirPath]!!.add("$dirPath/${line.substring(4)}")
-        } else {
-            val size = line.substringBefore(' ').toInt()
-            directories[dirPath] = (directories[dirPath] ?: 0) + size
-        }
-    }
+    val (directories, subDirectories) = navigateFileSystem(text)
 
     return directories.map { (key, _) -> totalSize(key, directories, subDirectories) }
         .filter { it <= 100000 }.sum()
@@ -41,8 +15,17 @@ fun totalSize(dir: String, directories: MutableMap<String, Int>, subDirectories:
     return directories[dir]!! + subDirectories[dir]!!.sumOf { totalSize(it, directories, subDirectories) }
 }
 
-
 fun solveB(text: String): Int {
+    val (directories, subDirectories) = navigateFileSystem(text)
+
+    val totalSize = totalSize("/", directories, subDirectories)
+    val free = 70000000 - totalSize
+    val needed = 30000000 - free
+    return directories.map { (key, _) -> totalSize(key, directories, subDirectories) }
+        .filter { it >= needed }.min()
+}
+
+private fun navigateFileSystem(text: String): Pair<MutableMap<String, Int>, MutableMap<String, MutableList<String>>> {
     val dirStack = Stack<String>()
     var dirPath = ""
     val directories = mutableMapOf<String, Int>()
@@ -70,10 +53,5 @@ fun solveB(text: String): Int {
             directories[dirPath] = (directories[dirPath] ?: 0) + size
         }
     }
-
-    val totalSize = totalSize("/", directories, subDirectories)
-    val free = 70000000 - totalSize
-    val needed = 30000000 - free
-    return directories.map { (key, _) -> totalSize(key, directories, subDirectories) }
-        .filter { it >= needed }.min()
+    return Pair(directories, subDirectories)
 }
