f40189715e4003a71eacfefba037fcc4e26bc837
Renette Ros <supergirl0894+git@gmail.com>
2022-12-09 09:32:19 +0200

    Clean up test data for day 9
    Signed-off-by: Renette Ros <supergirl0894+git@gmail.com>

--- src/test/kotlin/day9/Day9KtTest.kt
+++ src/test/kotlin/day9/Day9KtTest.kt
@@ -13,13 +13,13 @@ internal class Day9KtTest {
     fun sample1() {
         val text = """
             |R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2
+            |U 4
+            |L 3
+            |D 1
+            |R 4
+            |D 1
+            |L 5
+            |R 2
         """.trimMargin().trimEnd()
 
         assertEquals(13, solveA(text))
@@ -30,13 +30,13 @@ R 2
     fun sample2() {
         val text = """
             |R 5
-U 8
-L 8
-D 3
-R 17
-D 10
-L 25
-U 20""".trimMargin().trimEnd()
+            |U 8
+            |L 8
+            |D 3
+            |R 17
+            |D 10
+            |L 25
+            |U 20""".trimMargin().trimEnd()
 
         assertEquals(36, solveB(text))
     }
