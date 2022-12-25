0e4dffdd74cab1ee67c866906a6c16a58d79687f
Renette Ros <supergirl0894+git@gmail.com>
2022-12-25 07:58:51 +0200

    Solve day 25

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -2,11 +2,68 @@ package day25
 
 import helper.Debug
 
-fun solveA(text: String, debug: Debug = Debug.Disabled): Int {
-    return 0
+//What SNAFU number do you supply to Bob's console?
+fun solveA(text: String, debug: Debug = Debug.Disabled): String {
+    return toSnafu(text.lines().sumOf { parseSnafu(it) })
 }
 
+fun toSnafu(input: Long): String {
+    val base5Str = input.toString(5)
+    val digits = mutableListOf<Int>()
 
-fun solveB(text: String, debug: Debug = Debug.Disabled): Int {
-    return 0
+    val iterator = base5Str.map { it.digitToInt() }.reversed().iterator()
+    var overflowDigit = 0
+
+    while (iterator.hasNext()) {
+        val next = iterator.next()
+        val (digit, overflow) = snafuDigit(next + overflowDigit)
+        digits.add(digit)
+        overflowDigit = overflow
+    }
+
+    while (overflowDigit != 0) {
+        val (digit, overflow) = snafuDigit(overflowDigit)
+        digits.add(digit)
+        overflowDigit = overflow
+    }
+
+    val snafuNumber = digits.reversed().joinToString("") {
+        when (it) {
+            -1 -> "-"
+            -2 -> "="
+            else -> "$it"
+        }
+    }
+
+    val parseSnafu = parseSnafu(snafuNumber)
+    assert(parseSnafu == input) { "Snafu building for $input failed: $snafuNumber == $parseSnafu" }
+    return snafuNumber.trimStart('0')
+}
+
+/**
+ * Returns digit and overflow for a snafu digit
+ */
+private fun snafuDigit(digit: Int): Pair<Int, Int> = when {
+    digit >= 5 -> digit % 5 to digit / 5
+    digit == 4 -> -1 to 1
+    digit == 3 -> -2 to 1
+    else -> digit to 0
+}
+
+fun parseSnafu(it: String): Long {
+    val iterator = it.reversed().iterator()
+    var num = 0L
+    var n = 1L
+
+    while (iterator.hasNext()) {
+        val digit = when (val char = iterator.nextChar()) {
+            '-' -> -1
+            '=' -> -2
+            else -> char.digitToInt()
+        }
+        num += digit * n
+        n *= 5
+    }
+
+    return num
 }
--- src/test/kotlin/day25/Day25KtTest.kt
+++ src/test/kotlin/day25/Day25KtTest.kt
@@ -2,7 +2,6 @@ package day25
 
 import helper.Debug
 import helper.readDayFile
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -14,20 +13,48 @@ internal class Day25KtTest {
     @Test
     fun sample1() {
         val text = """
-            |
+            |1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122
         """.trimMargin().trimEnd()
 
-        assertEquals(0, solveA(text, Debug.Enabled))
-        assertEquals(0, solveB(text, Debug.Enabled))
+        assertEquals("2=-1=0", solveA(text, Debug.Enabled))
+//        assertEquals(0, solveB(text, Debug.Enabled))
     }
 
     @Test
-    @Ignore
-    fun sample2() {
-        val text = readDayFile(day, "sample2.in").readText().trimEnd()
+    fun testToSnafu() {
+        val list = listOf(
+            (1 to "1"),
+            (2 to "2"),
+            (3 to "1="),
+            (4 to "1-"),
+            (5 to "10"),
+            (6 to "11"),
+            (7 to "12"),
+            (8 to "2="),
+            (9 to "2-"),
+            (10 to "20"),
+            (15 to "1=0"),
+            (20 to "1-0"),
+            (2022 to "1=11-2"),
+            (12345 to "1-0---0"),
+            (314159265 to "1121-1110-1=0")
+        )
 
-        assertEquals(0, solveA(text, Debug.Enabled))
-        assertEquals(0, solveB(text, Debug.Disabled))
+        list.forEach { (int, expected) ->
+            assertEquals(expected, toSnafu(int.toLong()), "Wrong snafu number for $int")
+        }
     }
 
     @Test
@@ -36,10 +63,6 @@ internal class Day25KtTest {
 
         val solveA = solveA(lines)
         println("A: $solveA")
-        assertEquals(0, solveA)
-
-        val solveB = solveB(lines)
-        println("B: $solveB")
-        assertEquals(0, solveB)
+        assertEquals("2=1-=02-21===-21=200", solveA)
     }
 }
